b32fb81d02737ddf11b2532aad7358bf28ceabd2
RoccoCerro <rcerro02@gmail.com>
2024-06-12 17:08:21 +0200

    Add: call api with axios

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,17 @@
   <div>
     ciao mondo
   </div>
+  <AppMain />
 </template>
 
 <script>
+
+  import AppMain from './components/AppMain.vue'
+
   export default {
+    components: {
+      AppMain
+    },
     data(){
       return{
 
---
+++ src/components/AppMain.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Progetti</h1>
+  </div>
+</template>
+
+<script>
+  import axios from "axios";
+
+  export default {
+    data() {
+      return {
+        projects: [],
+      }
+    },
+    methods:{
+      fetchProjects(){
+        axios.get('http://127.0.0.1:8000/api/project',{
+          params: {
+            page: 1
+          }
+        })
+        .then(res => {
+          
+        })
+      }
+    },
+    created() {
+      this.fetchProjects();
+    }
+  }
+</script>
+
+<style>
+
+</style>
