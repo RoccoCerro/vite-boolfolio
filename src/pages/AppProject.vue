109fb7a5811cae952b08165fa7be489385a12c66
RoccoCerro <rcerro02@gmail.com>
2024-06-13 16:27:29 +0200

    Add: button go back in the AppProject

--- src/pages/AppProject.vue
+++ src/pages/AppProject.vue
@@ -3,13 +3,20 @@
     <div class="container">
       <h1>Dettaglio Progetto</h1>
       <p>{{ $route.params.slug }}</p>
+      <button @click="goBack" class="btn btn-outline-dark">
+        Indietro
+      </button>
     </div>
   </section>
 </template>
 
 <script>
   export default {
-    
+    methods:{
+      goBack(){
+        this.$router.back()
+      }
+    }
   }
 </script>
 
