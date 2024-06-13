72173bba568d9d5581f12e8410c218ddf5775a93
RoccoCerro <rcerro02@gmail.com>
2024-06-13 17:32:53 +0200

    Add: call to the api in app project

--- src/pages/AppProject.vue
+++ src/pages/AppProject.vue
@@ -3,6 +3,7 @@
     <div class="container">
       <h1>Dettaglio Progetto</h1>
       <p>{{ $route.params.slug }}</p>
+      <p>{{ console.log(project)  }}</p>
       <button @click="goBack" class="btn btn-outline-dark">
         Indietro
       </button>
@@ -11,11 +12,26 @@
 </template>
 
 <script>
+  import axios from 'axios'
   export default {
+    data(){
+      return{
+        project: []
+      }
+    },
     methods:{
       goBack(){
         this.$router.back()
+      },
+      fetchProject(){
+        axios.get('http://127.0.0.1:8000/api/project/' + this.$route.params.slug )
+        .then(res => {
+            this.project = res.data.results.data
+        })
       }
+    },
+    created(){
+      this.fetchProject();
     }
   }
 </script>
