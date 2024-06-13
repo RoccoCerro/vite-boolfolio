105063782ad0fd39c16881f245a8f7faa0e491c5
RoccoCerro <rcerro02@gmail.com>
2024-06-13 15:38:59 +0200

    Add: project in the view home

--- src/pages/AppPortfolio.vue
+++ src/pages/AppPortfolio.vue
@@ -1,15 +1,16 @@
 <template>
    <section class="my-portfolio">
-    <div class="container">
-      <h1 class="text-center">Questi sono i progetti</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam delectus totam est in aliquam, reiciendis animi quas esse ratione porro quasi aperiam voluptates earum dolore autem ipsa sequi iusto accusantium!</p>
-    </div>
+      <AppMain />
+
   </section>
 </template>
 
 <script>
+  import AppMain from '../components/AppMain.vue'
   export default {
-    
+    components:{
+      AppMain
+    }
   }
 </script>
 
